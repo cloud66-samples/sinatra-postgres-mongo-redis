252cc43877527716675229605a20390fdff61d88
DimitriosLisenko <DimitriosLisenko@users.noreply.github.com>
2016-01-07 16:18:02 +0000

    Update app.rb
    yaml

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require 'rack/throttle'
 require 'dalli'
 require 'redis'
 require 'resque'
+require 'yaml'
 
 use Rack::Parser, :content_types => {
   'application/json'  => Proc.new { |body| ::MultiJson.decode body }
